2b59be88adcf5f71e0f8428e0cea7849a2ac1f5f
Adam Wisher <37659188+mrwish7@users.noreply.github.com>
2024-05-18 08:21:16 +0100

    Slight weak to database schema
    Add some fields and a further table to database design

--- ordb.sql
+++ ordb.sql
@@ -6,6 +6,7 @@ CREATE TABLE `sites` (
   `detailed_name` text DEFAULT NULL,
   `lat` decimal(7,4) NOT NULL,
   `lon` decimal(7,4) NOT NULL,
+  `accurate` tinyint(1) DEFAULT 1,
   `last_updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   primary key (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -20,13 +21,23 @@ CREATE TABLE `fm_tx` (
   `erp_h` int DEFAULT '0',
   `erp_v` int DEFAULT '0',
   `ant_pattern` json DEFAULT NULL,
-  `station` text DEFAULT NULL,
-  `reg_prog` text DEFAULT NULL,
+  `callsign` text DEFAULT NULL,
+  `station_id` int NOT NULL,
   `pi` char(4) DEFAULT NULL,
   `pi_reg` char(4) DEFAULT NULL,
-  `ps` text DEFAULT NULL,
-  `ps_reg` text DEFAULT NULL,
+  `ps` json DEFAULT NULL,
+  `ps_reg` json DEFAULT NULL,
   `external_id` text DEFAULT NULL,
   `last_updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  primary key (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `stations` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` text DEFAULT NULL,
+  `reg_prog` text DEFAULT NULL,
+  `website` text DEFAULT NULL,
+  `streams` json DEFAULT NULL,
+  `logopath` text DEFAULT NULL,
   primary key (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
